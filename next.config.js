feb31d74175235e9d919ef550114e07b5821fb1b
nhannt112113 <nguyentrongnhan04072000@gmail.com>
2022-08-30 11:09:01 +0700

    apple

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['upload.wikimedia.org', 'rb.gy', 'cdn.sanity.io', 'lh3.googleusercontent.com', 'apps.googleusercontent.com'],
+  },
 }
